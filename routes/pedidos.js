8f7994db28a92c536da26aa00df596348694d720
Renato_O <renatorsdeoliveira>
2020-08-01 20:27:06 -0300

    Criando Rotas

---
+++ routes/pedidos.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Usando o GET dentro da rota de pedido'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: "Usando o Post da rota de pedido"
+    });
+});
+
+router.get('/:id_pedido', (res, req, next) => {
+    const id = req.params.id_produto
+    res.status(200).send({
+        mensagem: "Usando o GET de um pedido via id",
+        id: id
+    });
+});
+
+
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: "Usando o delete da rota de pedido"
+    });
+});
+
+
+
+module.exports = router;
---
+++ routes/produtos.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Usando o GET dentro da rota de produtos'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: "Usando o Post da rota de produtos"
+    });
+});
+
+router.get('/:id_produto', (res, req, next) => {
+    const id = req.params.id_produto
+    res.status(200).send({
+        mensagem: "Usando o GET de um produto via id",
+        id: id
+    });
+});
+
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: "Usando o pach da rota de produtos"
+    });
+});
+
+
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: "Usando o delete da rota de produtos"
+    });
+});
+
+
+
+module.exports = router;
