6bb0ad84f020a830be91cb84bc5d1160e5d13474
Renato_O <renatorsdeoliveira>
2020-08-02 07:20:08 -0300

    Feito a verificação dasrotas não encontradas

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
+
+app.use(morgan('dev'));
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+
+// Quando não encotrar rota
+app.use((res, req, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mesagem: error.message
+        }
+    })
+});
 
 module.exports = app;  
