a10e1238a8eafb491b913fbe2d558cd66c670e8d
Renato_O <renatorsdeoliveira>
2020-08-02 09:37:41 -0300

    Definindo o Header da API

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false})); // apresentar apenas dados simples
 app.use(bodyParser.json()); // só aceitará dados json no body
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 
+    'Origin, X-Requested-With, Content-Type, Accept,  Authorization');
+
+    if(req.method === 'OPTIONS'){
+        res.method('Access-Control-Allow-Methods', 'PUT', 'POST', 'DELETE', 'PATCH', 'GET')
+        return res.status(200).send({});
+    }
+    next();
+
+});
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
